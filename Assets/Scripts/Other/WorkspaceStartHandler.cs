58ded0af04661c6e7debbe1accc02bcbf582ae1e
Oseday <cancakir99@hotmail.com>
2020-08-13 05:30:27 +0300

    restedit

---
+++ Assets/Scripts/Other/WorkspaceStartHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkspaceStartHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject[] s = GameObject.FindGameObjectsWithTag("RemoveFromWorkspaceOnLoad");
+        foreach (var obj in s)
+        {
+            Destroy(obj);
+        }
+    }
+}
